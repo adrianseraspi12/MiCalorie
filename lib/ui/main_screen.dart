e457324a509d44f6ef46fd7696302f75f282a9eb
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-04-05 17:04:42 +0800

    - Changed Main Screen to show Search Food Screen

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,10 +1,9 @@
+import 'package:calorie_counter/ui/search_food_screen.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return SearchFoodScreen();
   }
 }
