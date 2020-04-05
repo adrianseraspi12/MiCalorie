151b5a200185f27514f446db6fc9adfca935d6a0
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-04-05 17:22:47 +0800

    - Added Abstract class Bloc to dispose stream

--- lib/data/nutrionx_service.dart
+++ lib/data/nutrionx_service.dart
@@ -3,7 +3,7 @@ import 'package:chopper/chopper.dart';
 @ChopperApi(baseUrl: "/search")
 abstract class NutritionxSearchService extends ChopperService {
 
-  static NutritionxSearchService create([ChopperClient client]) => _$NutritionxSearchService(client);
+  static NutritionxSearchService create([ChopperClient client]) => _NutritionxSearchService(client);
 
   @Get(path: "instant/{detailed}/{query}")
   Future<Response> getListOfSearchFood(@Path('query') String food, @Path('detailed') bool isShowDetails);
