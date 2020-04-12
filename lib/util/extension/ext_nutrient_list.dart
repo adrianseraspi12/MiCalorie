8415f0dda65927c56bd47e6348bd5556e2a8c614
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-04-12 16:46:42 +0800

    - Added Extensions For Getting Nutrient Type

---
+++ lib/util/extension/ext_nutrient_list.dart
@@ -0,0 +1,28 @@
+import 'package:calorie_counter/data/model/list_of_food.dart';
+
+extension NutrientList on List<Nutrients> {
+
+  static const Map<NutrientType, int> _nutrientTypeAttrId = {
+    NutrientType.carbs: 205,
+    NutrientType.fat: 204,
+    NutrientType.protein: 203
+  };
+
+  double getNutrient(NutrientType type) {
+    final attrId = _nutrientTypeAttrId[type];
+    final nutrient =  this.firstWhere((element) {
+      return element.attrId == attrId;
+    });
+
+    return nutrient.value;
+  }
+    
+}
+    
+enum NutrientType {
+  
+  carbs,
+  fat,
+  protein,
+
+}
