424cd86c291ba2cceca10d85183de393bd485e4f
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-04-18 18:09:00 +0800

    - Added SnackNutrients Entity

---
+++ lib/data/local/entity/snack_nutrients.dart
@@ -0,0 +1,15 @@
+import 'package:floor/floor.dart';
+
+@Entity(tableName: 'snack_nutrients')
+class SnackNutrients {
+
+  @primaryKey
+  final int id;
+  final int calories;
+  final double carbs;
+  final double fat;
+  final double protein;
+
+  SnackNutrients(this.id, this.calories, this.carbs, this.fat, this.protein);
+  
+}
