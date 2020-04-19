4233d581f60061ba4317d54bc1a48a09fddf2f80
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-04-19 19:24:24 +0800

    - Changed Showing Daily Summary Screen

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,28 +1,9 @@
-import 'package:calorie_counter/bloc/bloc_provider.dart';
-import 'package:calorie_counter/bloc/food_bloc.dart';
-import 'package:calorie_counter/data/model/list_of_food.dart';
-import 'package:calorie_counter/ui/search_food_screen.dart';
+import 'package:calorie_counter/ui/daily_summary_screen.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<ListOfFood>(
-      
-      stream: BlocProvider.of<FoodBloc>(context).listOfFoodStream,
-      
-      builder: (context, snapshot) {
-        final listOfFood = snapshot.data;
-
-        if (listOfFood == null) {
-          return SearchFoodScreen();
-        }
-        else {
-          return Container();
-        }
-
-      },
-      
-      );
+    return DailySummaryScreen();
   }
 }
