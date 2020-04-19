8cb10edf065c8c55c06ade2c7611d1e01be68b24
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-04-19 19:24:00 +0800

    - Removed BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
-import 'package:calorie_counter/bloc/food_bloc.dart';
 import 'package:calorie_counter/ui/main_screen.dart';
 import 'package:flutter/material.dart';
 
-import 'bloc/bloc_provider.dart';
-
 void main() {
   runApp(
-    BlocProvider<FoodBloc>(
-      bloc: FoodBloc(),
-      child: MaterialApp(
-        title: 'Calorie Counter',
-        theme: ThemeData(
-          primaryColor: Colors.red[400]
-        ),
-        home: MainScreen(),
+    MaterialApp(
+      title: 'Calorie Counter',
+      theme: ThemeData(
+        primaryColor: Colors.red[400]
       ),
+      home: MainScreen(),
     )
   );
 }
