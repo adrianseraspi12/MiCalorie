3b1acc6aae7b46dbe295c5b2c2343984f6376e69
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-04-20 21:13:30 +0800

    - Added Meal Summary

---
+++ lib/data/model/meal_summary.dart
@@ -0,0 +1,11 @@
+class MealSummary {
+
+  String name;
+  int calories;
+  double carbs;
+  double fat;
+  double protein;
+
+  MealSummary(this.name, this.calories, this.carbs, this.fat, this.protein);
+
+}
