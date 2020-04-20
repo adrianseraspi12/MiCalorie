a7acfa60a9748e62ed5e3cbb31e4fc77e3639037
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-04-20 21:15:26 +0800

    - Changed Manual Creation of Cards to ListView

--- lib/ui/daily_summary_screen.dart
+++ lib/ui/daily_summary_screen.dart
@@ -1,7 +1,7 @@
 import 'package:calorie_counter/bloc/bloc_provider.dart';
 import 'package:calorie_counter/bloc/daily_summary_bloc.dart';
-import 'package:calorie_counter/data/local/entity/breakfast_nutrients.dart';
 import 'package:calorie_counter/data/local/entity/total_nutrients_per_day.dart';
+import 'package:calorie_counter/data/model/meal_summary.dart';
 import 'package:flutter/material.dart';
 
 class DailySummaryScreen extends StatelessWidget {
@@ -16,188 +16,186 @@ class DailySummaryScreen extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(title: Text('Daily Summary')),
 
-        body: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-
-            _buildTotalNutrients(bloc),
-            _buildBreakfast(bloc)
-
-          ],
-        )
+        body: _buildResult(bloc)
       ),
     );
   }
 
-  Widget _buildTotalNutrients(DailySummaryBloc bloc) {
-    return StreamBuilder<TotalNutrientsPerDay>(
-      stream: bloc.totalNutrientsPerDayStream,
+  Widget _buildResult(DailySummaryBloc bloc) {
+    return StreamBuilder<DailySummaryResult>(
+      stream: bloc.dailySummaryResultStream,
       builder: (context, snapshot) {
-        final totalNutrients = snapshot.data;
-        
-        if (totalNutrients == null) {
-          return Center(child: Text('DATA UNAVAILABLE'),);
+
+        final dailySummaryResult = snapshot.data;
+
+        if (dailySummaryResult == null) {
+          return Center(child: Text('DATA UNAVAILABLE'));
         }
         else {
-          return Card(
-            margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
-            child: Container(
-              padding: EdgeInsets.all(16.0),
-              child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
+          return ListView.builder(
+            itemCount: dailySummaryResult.mealSummary.length + 1,
+            itemBuilder: (context, index) {
 
-                Column(children: <Widget>[
-                  Text(
-                    '${totalNutrients.carbs}',
-                    style: TextStyle(fontSize: 32.0),),
-                  Text(
-                    'Carbs',
-                    style: TextStyle(fontSize: 16.0))
-                ],),
+              if (index == 0) {
+                return _buildTotalNutrients(dailySummaryResult.totalNutrientsPerDay);
+              }
 
-                Column(children: <Widget>[
-                  Text(
-                    '${totalNutrients.fat}',
-                    style: TextStyle(fontSize: 32.0)),
-                  Text(
-                    'Fat',
-                    style: TextStyle(fontSize: 16.0))
-                ],),
+              index -= 1;
 
-                Column(children: <Widget>[
-                  Text(
-                    '${totalNutrients.protein}',
-                    style: TextStyle(fontSize: 32.0)),
-                  Text(
-                    'Protein',
-                    style: TextStyle(fontSize: 16.0))
-                ],),
+              var mealSummary = dailySummaryResult.mealSummary[index];
 
-                Column(children: <Widget>[
-                  Text(
-                    '${totalNutrients.calories}',
-                    style: TextStyle(fontSize: 32.0)),
-                  Text(
-                    'Calories',
-                    style: TextStyle(fontSize: 16.0))
-                ],),
+              return _buildMealSummary(mealSummary);
 
-              ],
-            ),),
-          );
+            });
         }
 
-      },
-      );
+      });
   }
 
-  Widget _buildBreakfast(DailySummaryBloc bloc) {
-    return StreamBuilder<BreakfastNutrients>(
-      stream: bloc.breakfastNutrientsStream,
-      builder: (context, snapshot) {
+  Widget _buildTotalNutrients(TotalNutrientsPerDay totalNutrients) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
 
-        // final breakfastNutrients = snapshot.data;
-        final breakfastNutrients = BreakfastNutrients(0, 0, 0, 0, 0);
-        
-        // if (breakfastNutrients == null) {
-        //   return Center(child: Text('DATA UNAVAILABLE'),);
-        // }
-        // else {
-          return Card(
-            margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Container(
-              child: Column(
-                
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(16.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          'Breakfast',
-                          style: TextStyle(fontSize: 32.0)),
-                        
-                        Text(
-                          '${breakfastNutrients.calories}',
-                          style: TextStyle(fontSize: 32.0)),
+          Column(children: <Widget>[
+            Text(
+              '${totalNutrients.carbs}',
+              style: TextStyle(fontSize: 32.0),),
+            Text(
+              'Carbs',
+              style: TextStyle(fontSize: 16.0))
+          ],),
 
-                      ],
-                    ),
-                  ),
+          Column(children: <Widget>[
+            Text(
+              '${totalNutrients.fat}',
+              style: TextStyle(fontSize: 32.0)),
+            Text(
+              'Fat',
+              style: TextStyle(fontSize: 16.0))
+          ],),
 
-                  Divider(),
+          Column(children: <Widget>[
+            Text(
+              '${totalNutrients.protein}',
+              style: TextStyle(fontSize: 32.0)),
+            Text(
+              'Protein',
+              style: TextStyle(fontSize: 16.0))
+          ],),
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          Column(children: <Widget>[
+            Text(
+              '${totalNutrients.calories}',
+              style: TextStyle(fontSize: 32.0)),
+            Text(
+              'Calories',
+              style: TextStyle(fontSize: 16.0))
+          ],),
+
+        ],
+      ),),
+    );
+  }
+
+  Widget _buildMealSummary(MealSummary mealSummary) {
+    return Material(
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: InkWell(
+          onTap: () {},
+          child: Container(
+            child: Column(
+              
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-
-                      Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-
-                        Container(
-                          padding: EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
-                          child: Column(
-                            children: <Widget>[
-                              Text(
-                                '${breakfastNutrients.carbs}',
-                                style: TextStyle(fontSize: 32.0),),
-                              Text(
-                                'Carbs',
-                                style: TextStyle(fontSize: 16.0))
-                            ],
-                          ),
-                        ),
-
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-                          child: Column(
-                            children: <Widget>[
-                              Text(
-                                '${breakfastNutrients.fat}',
-                                style: TextStyle(fontSize: 32.0),),
-                              Text(
-                                'Fat',
-                                style: TextStyle(fontSize: 16.0))
-                            ],
-                          ),
-                        ),
-
-                        Container(
-                          padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-                          child: Column(
-                            children: <Widget>[
-                              Text(
-                                '${breakfastNutrients.protein}',
-                                style: TextStyle(fontSize: 32.0),),
-                              Text(
-                                'Protein',
-                                style: TextStyle(fontSize: 16.0))
-                            ],
-                          ),
-                        ),
-
-                      ],
-                        ),
-
-                      Expanded(child: Text('VIEW DETAILS'))
+                      Text(
+                        '${mealSummary.name}',
+                        style: TextStyle(fontSize: 32.0)),
+                      
+                      Text(
+                        '${mealSummary.calories}',
+                        style: TextStyle(fontSize: 32.0)),
 
                     ],
                   ),
+                ),
 
-                ],
+                Divider(),
 
-              )
-            ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
 
-          );
+                    Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
 
-        // }
+                      Container(
+                        padding: EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              '${mealSummary.carbs}',
+                              style: TextStyle(fontSize: 32.0),),
+                            Text(
+                              'Carbs',
+                              style: TextStyle(fontSize: 16.0))
+                          ],
+                        ),
+                      ),
 
-      },
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              '${mealSummary.fat}',
+                              style: TextStyle(fontSize: 32.0),),
+                            Text(
+                              'Fat',
+                              style: TextStyle(fontSize: 16.0))
+                          ],
+                        ),
+                      ),
 
+                      Container(
+                        padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              '${mealSummary.protein}',
+                              style: TextStyle(fontSize: 32.0),),
+                            Text(
+                              'Protein',
+                              style: TextStyle(fontSize: 16.0))
+                          ],
+                        ),
+                      ),
+
+                    ],
+                      ),
+
+                    Expanded(child: Text('VIEW DETAILS'))
+
+                  ],
+                ),
+
+              ],
+
+            )
+          ),
+        ),
+
+      ),
     );
   }
 
