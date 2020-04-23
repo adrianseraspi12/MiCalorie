733365efaf09d3a9c467a795d5c4b261c9b6b5e5
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-04-23 21:50:29 +0800

    - Removed all meal Id

--- lib/data/local/entity/total_nutrients_per_day.dart
+++ lib/data/local/entity/total_nutrients_per_day.dart
@@ -1,29 +1,6 @@
 import 'package:floor/floor.dart';
 
-import 'dinner_nutrients.dart';
-import 'lunch_nutrients.dart';
-import 'snack_nutrients.dart';
-import 'breakfast_nutrients.dart';
-
-@Entity(tableName: 'total_nutrients_per_day',
-        foreignKeys: [
-          ForeignKey(childColumns: ['breakfast_id'],
-                    parentColumns: ['id'], 
-                    entity: BreakfastNutrients),
-
-          ForeignKey(childColumns: ['lunch_id'],
-                    parentColumns: ['id'], 
-                    entity: LunchNutrients),
-
-          ForeignKey(childColumns: ['dinner_id'],
-                    parentColumns: ['id'], 
-                    entity: DinnerNutrients),
-
-          ForeignKey(childColumns: ['snack_id'],
-                    parentColumns: ['id'], 
-                    entity: SnackNutrients),
-          
-        ])
+@Entity(tableName: 'total_nutrients_per_day')
 class TotalNutrientsPerDay {
 
   @primaryKey
@@ -34,23 +11,7 @@ class TotalNutrientsPerDay {
   final double fat;
   final double protein;
 
-  @ColumnInfo(name: 'breakfast_id')
-  final int breakfastId;
-
-  @ColumnInfo(name: 'lunch_id')
-  final int lunchId;
-
-  @ColumnInfo(name: 'dinner_id')
-  final int dinnerId;
-
-  @ColumnInfo(name: 'snack_id')
-  final int snackId;
-
   TotalNutrientsPerDay(this.id,
-                      this.breakfastId,
-                      this.lunchId,
-                      this.dinnerId,
-                      this.snackId,
                       this.date, 
                       this.calories,
                       this.carbs,
