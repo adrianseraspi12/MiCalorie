657baecb417ae06c192939db8bd197b041ade279
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-04-25 17:42:00 +0800

    - Added TotalNurientsId and Date

--- lib/data/model/meal_summary.dart
+++ lib/data/model/meal_summary.dart
@@ -6,7 +6,16 @@ class MealSummary {
   double carbs;
   double fat;
   double protein;
+  String date;
+  int totalNutrientsId;
 
-  MealSummary(this.id, this.name, this.calories, this.carbs, this.fat, this.protein);
+  MealSummary(this.id,
+  this.name, 
+  this.calories,
+  this.carbs, 
+  this.fat, 
+  this.protein,
+  this.date,
+  this.totalNutrientsId);
 
 }
