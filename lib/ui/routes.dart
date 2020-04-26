1575e9d1daf1da1c1414fd3b40c2f2f0405d68bb
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-04-26 17:04:31 +0800

    - Added Routes Name

---
+++ lib/ui/routes.dart
@@ -0,0 +1,8 @@
+class Routes {
+
+  static const String dailySummaryScreen = '/';
+  static const String mealFoodListScreen = '/mealFoodListScreen';
+  static const String searchFoodScreen = '/searchFoodScreen';
+  static const String foodDetailsScreen = '/mealFoodListScreen/searchFoodScreen/foodDetailsScreen';
+
+}
