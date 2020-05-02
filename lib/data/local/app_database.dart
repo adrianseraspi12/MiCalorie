fba16c98e5e96ac82331a1be83e9d53120ea570a
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-05-02 12:02:21 +0800

    - Removed unused objects

--- lib/data/local/app_database.dart
+++ lib/data/local/app_database.dart
@@ -5,38 +5,21 @@ import 'package:sqflite/sqflite.dart' as sqflite;
 import 'package:path/path.dart';
 
 import 'package:floor/floor.dart';
-
-import 'package:calorie_counter/data/local/dao/breakfast_nutrients_dao.dart';
-import 'package:calorie_counter/data/local/dao/dinner_nutrients_dao.dart';
-import 'package:calorie_counter/data/local/dao/food_dao.dart';
-import 'package:calorie_counter/data/local/dao/lunch_nutrients_dao.dart';
-import 'package:calorie_counter/data/local/dao/snack_nutrients_dao.dart';
 import 'package:calorie_counter/data/local/entity/food.dart';
-import 'package:calorie_counter/data/local/entity/breakfast_nutrients.dart';
-import 'package:calorie_counter/data/local/entity/dinner_nutrients.dart';
-import 'package:calorie_counter/data/local/entity/lunch_nutrients.dart';
-import 'package:calorie_counter/data/local/entity/snack_nutrients.dart';
 import 'package:calorie_counter/data/local/entity/total_nutrients_per_day.dart';
 
+import 'dao/food_dao.dart';
 import 'entity/meal_nutrients.dart';
 
 part 'app_database.g.dart';
 
 @Database(version: 1, entities: [TotalNutrientsPerDay, 
                                 MealNutrients,
-                                BreakfastNutrients,
-                                LunchNutrients,
-                                DinnerNutrients,
-                                SnackNutrients,
                                 Food])
 abstract class AppDatabase extends FloorDatabase {
 
   TotalNutrientsPerDayDao get totalNutrientsPerDayDao;
   MealNutrientsDao get mealNutrientsDao;
-  BreakfastNutrientsDao get breakfastNutrientsDao;
-  LunchNutrientsDao get lunchNutrientsDao;
-  DinnerNutrientsDao get dinnerNutrientsDao;
-  SnackNutrientsDao get snackNutrientsDao;
   FoodDao get foodDao;
 
   static Future<AppDatabase> getInstance() async {
