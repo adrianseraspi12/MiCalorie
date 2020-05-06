45e089f461f14045a03a9f528d206af3ed508688
Adrian Jun Seraspi <abseraspi@htechcorp.net>
2020-05-06 21:20:39 +0800

    - Implemented Neumorphism UI

--- lib/ui/daily_summary_screen.dart
+++ lib/ui/daily_summary_screen.dart
@@ -2,10 +2,14 @@ import 'package:calorie_counter/bloc/bloc_provider.dart';
 import 'package:calorie_counter/bloc/daily_summary_bloc.dart';
 import 'package:calorie_counter/data/local/entity/meal_nutrients.dart';
 import 'package:calorie_counter/data/local/entity/total_nutrients_per_day.dart';
+import 'package:calorie_counter/ui/widgets/circular_view.dart';
+import 'package:calorie_counter/ui/widgets/nutrient_pie_chart_view.dart';
+import 'package:calorie_counter/ui/widgets/pie_chart_view.dart';
 import 'package:calorie_counter/util/extension/ext_meal_type_description.dart';
 import 'package:calorie_counter/ui/meal_food_list_screen.dart';
 import 'package:calorie_counter/util/constant/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class DailySummaryScreen extends StatefulWidget {
   
@@ -24,10 +28,8 @@ class _DailySummaryScreenState extends State<DailySummaryScreen> {
     return BlocProvider<DailySummaryBloc>(
       bloc: bloc,
       child: Scaffold(
-        appBar: AppBar(
-          title: _buildAppBarDate(context, bloc),
-        ),
-        body: _buildResult(bloc)
+        backgroundColor: Color.fromRGBO(193,214,233, 1),
+        body: _buildResult(context, bloc)
       ),
     );
   }
@@ -48,26 +50,83 @@ class _DailySummaryScreenState extends State<DailySummaryScreen> {
         }
 
         return Container(
-          child: FlatButton(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Text(
-              '$date',
-              style: TextStyle(
-                fontSize: 24.0,
-                color: Colors.white)
+          margin: EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget> [
+              Center(
+                child: FittedBox(
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 16.0),
+                    child: Text(
+                      'Daily Summary',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
               ),
 
-              Icon(
-                Icons.keyboard_arrow_down, 
-                color: Colors.white,
-              ),
+              NeumorphicButton(
+                padding: EdgeInsets.all(16.0),
+                margin: EdgeInsets.symmetric(horizontal: 60.0, vertical: 8.0),
+                onClick: () { 
+                  _buildDatePicker(context, bloc);
+                },
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.convex,
+                  shadowDarkColorEmboss: Color.fromRGBO(163,177,198, 1),
+                  shadowLightColorEmboss: Colors.white,
+                  color: Color.fromRGBO(193,214,233, 1),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'Date:',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
 
-            ]),
-            onPressed: () {
-              _buildDatePicker(context, bloc);
-            },
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          margin: EdgeInsets.symmetric(horizontal: 8.0),
+                          child: FittedBox(
+                            child: Text(
+                              '$date',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 16,
+                                fontWeight: FontWeight.w900,
+                                ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Neumorphic(
+                      boxShape: NeumorphicBoxShape.circle(),
+                      child: Icon(Icons.chevron_right),
+                      style: NeumorphicStyle(
+                        shape: NeumorphicShape.convex,
+                        shadowDarkColorEmboss: Color.fromRGBO(163,177,198, 1),
+                        shadowLightColorEmboss: Colors.white,
+                        color: Color.fromRGBO(193,214,233, 1),
+                      ),
+                    )
+
+                  ],
+                ),
+              ),
+              
+            ]
           ),
         );
       }
@@ -88,30 +147,48 @@ class _DailySummaryScreenState extends State<DailySummaryScreen> {
 
   }
 
-  Widget _buildResult(DailySummaryBloc bloc) {
+  Widget _buildResult(BuildContext context, DailySummaryBloc bloc) {
     return StreamBuilder<DailySummaryResult>(
       stream: bloc.dailySummaryResultStream,
       builder: (context, snapshot) {
 
         final dailySummaryResult = snapshot.data;
 
+        final calories = dailySummaryResult.totalNutrientsPerDay.calories;
+        final carbs = dailySummaryResult.totalNutrientsPerDay.carbs;
+        final fat = dailySummaryResult.totalNutrientsPerDay.fat;
+        final protein = dailySummaryResult.totalNutrientsPerDay.protein;
+
         if (dailySummaryResult == null) {
           return Center(child: Text('DATA UNAVAILABLE'));
         }
         else {
           return ListView.builder(
-            itemCount: dailySummaryResult.mealNutrients.length + 1,
+            itemCount: dailySummaryResult.mealNutrients.length + 2,
             itemBuilder: (context, index) {
 
               if (index == 0) {
-                return _buildTotalNutrients(dailySummaryResult.totalNutrientsPerDay);
+                return _buildAppBarDate(context, bloc);
+              }
+              else if (index == 1) {
+                return Container(
+                  margin: EdgeInsets.all(16.0),
+                  child: NutrientPieChartView(
+                    calories: calories,
+                    carbs: carbs, 
+                    fat: fat, 
+                    protein: protein)
+                );
               }
 
-              index -= 1;
+              index -= 2;
 
               var mealNutrients = dailySummaryResult.mealNutrients[index];
 
-              return _buildMealSummary(context, bloc, mealNutrients);
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 8.0),
+                child: _buildMealSummary(context, bloc, mealNutrients)
+              );
 
             });
         }
@@ -119,150 +196,184 @@ class _DailySummaryScreenState extends State<DailySummaryScreen> {
       });
   }
 
-  Widget _buildTotalNutrients(TotalNutrientsPerDay totalNutrients) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
-      child: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-
-          Column(children: <Widget>[
-            Text(
-              '${totalNutrients.carbs}',
-              style: TextStyle(fontSize: 32.0),),
-            Text(
-              'Carbs',
-              style: TextStyle(fontSize: 16.0))
-          ],),
-
-          Column(children: <Widget>[
-            Text(
-              '${totalNutrients.fat}',
-              style: TextStyle(fontSize: 32.0)),
-            Text(
-              'Fat',
-              style: TextStyle(fontSize: 16.0))
-          ],),
-
-          Column(children: <Widget>[
-            Text(
-              '${totalNutrients.protein}',
-              style: TextStyle(fontSize: 32.0)),
-            Text(
-              'Protein',
-              style: TextStyle(fontSize: 16.0))
-          ],),
-
-          Column(children: <Widget>[
-            Text(
-              '${totalNutrients.calories}',
-              style: TextStyle(fontSize: 32.0)),
-            Text(
-              'Calories',
-              style: TextStyle(fontSize: 16.0))
-          ],),
-
-        ],
-      ),),
-    );
-  }
-
   Widget _buildMealSummary(BuildContext context, DailySummaryBloc bloc, MealNutrients mealNutrients) {
-    return Material(
-      child: Card(
-        margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        child: InkWell(
-          onTap: () {
-            _showMealFoodListScreen(context, mealNutrients, bloc);
-          },
-          child: Container(
-            child: Column(
-              
+    
+    return NeumorphicButton(
+      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      onClick: () => _showMealFoodListScreen(context, mealNutrients, bloc),
+      style: NeumorphicStyle(
+        shape: NeumorphicShape.convex,
+        shadowDarkColorEmboss: Color.fromRGBO(163,177,198, 1),
+        shadowLightColorEmboss: Colors.white,
+        color: Color.fromRGBO(193,214,233, 1),
+      ),
+      child: Column(
+        
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        '${mealNutrients.type.description()}',
-                        style: TextStyle(fontSize: 32.0)),
-                      
-                      Text(
+                Text(
+                  '${mealNutrients.type.description()}',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w700,
+                  )
+                ),
+                
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: FittedBox(
+                      child: Text(
                         '${mealNutrients.calories}',
-                        style: TextStyle(fontSize: 32.0)),
-
-                    ],
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.w700,
+                        )
+                      ),
+                    ),
                   ),
                 ),
 
-                Divider(),
+              ],
+            ),
+          ),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          Flex(
+            direction: Axis.horizontal,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+
+              Expanded(
+                flex: 9,
+                child: Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
 
-                    Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-
-                      Container(
-                        padding: EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
-                        child: Column(
-                          children: <Widget>[
-                            Text(
-                              '${mealNutrients.carbs}',
-                              style: TextStyle(fontSize: 32.0),),
-                            Text(
-                              'Carbs',
-                              style: TextStyle(fontSize: 16.0))
-                          ],
+                    Expanded(
+                      flex: 1,
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                          child: Column(
+                            children: <Widget>[
+                              FittedBox(
+                                child: Text(
+                                  '${mealNutrients.carbs}g',
+                                  style: TextStyle(
+                                    fontFamily: 'Roboto',
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                'Carbs',
+                                style: TextStyle(
+                                  fontFamily: 'Roboto',
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
+                    ),
 
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-                        child: Column(
-                          children: <Widget>[
-                            Text(
-                              '${mealNutrients.fat}',
-                              style: TextStyle(fontSize: 32.0),),
-                            Text(
-                              'Fat',
-                              style: TextStyle(fontSize: 16.0))
-                          ],
+                    Expanded(
+                      flex: 1,
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                          child: Column(
+                            children: <Widget>[
+                              FittedBox(
+                                child: Text(
+                                  '${mealNutrients.fat}g',
+                                  style: TextStyle(
+                                    fontFamily: 'Roboto',
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                'Fat',
+                                style: TextStyle(
+                                  fontFamily: 'Roboto',
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
+                    ),
 
-                      Container(
-                        padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-                        child: Column(
-                          children: <Widget>[
-                            Text(
-                              '${mealNutrients.protein}',
-                              style: TextStyle(fontSize: 32.0),),
-                            Text(
-                              'Protein',
-                              style: TextStyle(fontSize: 16.0))
-                          ],
+                    Expanded(
+                      flex: 1,
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
+                          child: Column(
+                            children: <Widget>[
+                              FittedBox(
+                                child: Text(
+                                  '${mealNutrients.protein}g',
+                                  style: TextStyle(
+                                    fontFamily: 'Roboto',
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                'Protein',
+                                style: TextStyle(
+                                  fontFamily: 'Roboto',
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
-
-                    ],
-                      ),
-
-                    Expanded(child: Text('VIEW DETAILS'))
+                    ),
 
                   ],
                 ),
+              ),
 
-              ],
+              Expanded(
+                flex: 1,
+                child: Neumorphic(
+                  boxShape: NeumorphicBoxShape.circle(),
+                  child: Icon(Icons.chevron_right),
+                  style: NeumorphicStyle(
+                    shape: NeumorphicShape.convex,
+                    shadowDarkColorEmboss: Color.fromRGBO(163,177,198, 1),
+                    shadowLightColorEmboss: Colors.white,
+                    color: Color.fromRGBO(193,214,233, 1),
+                  ),
+                ),
+              )
 
-            )
+            ],
           ),
-        ),
+
+        ],
 
       ),
     );
