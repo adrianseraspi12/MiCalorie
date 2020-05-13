09a8f55bce3246c8144107213f294a22e423abb5
Adrian Jun Seraspi <adrianseraspi12@gmail.com>
2020-05-13 21:08:35 +0800

    - Added quick add food route name

--- lib/util/constant/routes.dart
+++ lib/util/constant/routes.dart
@@ -4,5 +4,6 @@ class Routes {
   static const String mealFoodListScreen = '/mealFoodListScreen';
   static const String searchFoodScreen = '/searchFoodScreen';
   static const String foodDetailsScreen = '/mealFoodListScreen/searchFoodScreen/foodDetailsScreen';
+  static const String quickAddFoodScreen = '/quickAddFoodScreen';
 
 }
