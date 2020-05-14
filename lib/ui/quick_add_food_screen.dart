73063a9746ccc50b1fee79302162ed5530610c73
Adrian Jun Seraspi <adrianseraspi12@gmail.com>
2020-05-14 08:42:44 +0800

    - Changed Textfield UI

--- lib/ui/quick_add_food_screen.dart
+++ lib/ui/quick_add_food_screen.dart
@@ -4,8 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class QuickAddFoodScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       backgroundColor: Color.fromRGBO(193,214,233, 1),
       body: SafeArea(
@@ -53,13 +56,97 @@ class QuickAddFoodScreen extends StatelessWidget {
       child: Column(
         children: <Widget>[
 
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          _buildTextfield(
+            title: 'Name :', 
+            textHint: 'Food'
+          ),
+
+          _buildTextfield(
+            title: 'Brand :', 
+            textHint: '(Optional)'
+          ),
+
+          _buildTextfield(
+            title: 'Quantity :', 
+            text: '1', 
+            textInputType: TextInputType.number,
+            onChanged: (quantity) {
+              //  add bloc here to compute quantity
+            },
+            onEditingComplete: (quantity) {
+              //  add bloc here to compute quantity
+            }
+          ),
+
+          _buildTextfield(
+            title: 'Calories :', 
+            text: '0', 
+            textInputType: TextInputType.number
+          ),
+
+          _buildTextfield(
+            title: 'Carbs :', 
+            text: '0', 
+            textInputType: TextInputType.number
+          ),
+
+          _buildTextfield(
+            title: 'Fat :', 
+            text: '0', 
+            textInputType: TextInputType.number
+          ),
+
+          _buildTextfield(
+            title: 'Protein :', 
+            text: '0', 
+            textInputType: TextInputType.number
+          ),
+
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextfield({
+    Key key,
+    String title, 
+    String text = '', 
+    TextInputType textInputType,
+    String textHint,
+    Function(String) onChanged,
+    Function(String) onEditingComplete,
+  }) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Flex(
+        direction: Axis.horizontal,
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                )
+              ),
+            ),
+          ),
+
+
+          Expanded(
+            flex: 8,
             child: NeumorphicTextfield(
-              textInputAction: TextInputAction.done,
+              onChanged: onChanged,
+              onEditingComplete: onEditingComplete,
+              text: text,
+              textInputAction:  TextInputAction.done,
+              textInputType: textInputType,
               padding: EdgeInsets.all(8.0),
               decoration: InputDecoration(
-                hintText: 'Food',
+                hintText: textHint,
                 border: InputBorder.none,
                 hintStyle: TextStyle(
                   fontFamily: 'Roboto',
@@ -67,193 +154,8 @@ class QuickAddFoodScreen extends StatelessWidget {
                   fontWeight: FontWeight.w400,
                 ),
               ),
-              leading: Container(
-                margin: EdgeInsets.only(left: 16.0),
-                child: Text(
-                  'Name :',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  )
-                ),
-              ),
             ),
           ),
-
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: NeumorphicTextfield(
-              textInputAction: TextInputAction.done,
-              padding: EdgeInsets.all(8.0),
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: '(Optional)',
-                hintStyle: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              leading: Container(
-                margin: EdgeInsets.only(left: 16.0),
-                child: Text(
-                  'Brand :',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  )
-                ),
-              ),
-            ),
-          ),
-
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: NeumorphicTextfield(
-              text: '1',
-              textInputType: TextInputType.number,
-              textInputAction: TextInputAction.done,
-              padding: EdgeInsets.all(8.0),
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintStyle: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              leading: Container(
-                margin: EdgeInsets.only(left: 16.0),
-                child: Text(
-                  'Quantity :',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  )
-                ),
-              ),
-            ),
-          ),
-
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: NeumorphicTextfield(
-              text: '0',
-              textInputType: TextInputType.number,
-              textInputAction: TextInputAction.done,
-              padding: EdgeInsets.all(8.0),
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintStyle: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              leading: Container(
-                margin: EdgeInsets.only(left: 16.0),
-                child: Text(
-                  'Calories :',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  )
-                ),
-              ),
-            ),
-          ),
-
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: NeumorphicTextfield(
-              text: '0',
-              textInputType: TextInputType.number,
-              textInputAction: TextInputAction.done,
-              padding: EdgeInsets.all(8.0),
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintStyle: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              leading: Container(
-                margin: EdgeInsets.only(left: 16.0),
-                child: Text(
-                  'Carbs :',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  )
-                ),
-              ),
-            ),
-          ),
-
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: NeumorphicTextfield(
-              text: '0',
-              textInputType: TextInputType.number,
-              textInputAction: TextInputAction.done,            
-              padding: EdgeInsets.all(8.0),
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintStyle: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              leading: Container(
-                margin: EdgeInsets.only(left: 16.0),
-                child: Text(
-                  'Fat :',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  )
-                ),
-              ),
-            ),
-          ),
-
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: NeumorphicTextfield(
-              text: '0',
-              textInputType: TextInputType.numberWithOptions(signed: true, decimal: false),
-              textInputAction: TextInputAction.done,
-              padding: EdgeInsets.all(8.0),
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintStyle: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              leading: Container(
-                margin: EdgeInsets.only(left: 16.0),
-                child: Text(
-                  'Protein :',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  )
-                ),
-              ),
-            ),
-          ),
-
         ],
       ),
     );
