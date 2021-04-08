0d04ca48e00ffda4966c296298c185462d8c9646
AJ Seraspi <adrianseraspi12@gmail.com>
2021-04-08 21:04:50 +0800

    - Added and implement MealSummaryView Row

--- lib/ui/screens/daily_summary_screen.dart
+++ lib/ui/screens/daily_summary_screen.dart
@@ -2,8 +2,8 @@ import 'package:calorie_counter/bloc/daily_summary/daily_summary_bloc.dart';
 import 'package:calorie_counter/data/local/app_database.dart';
 import 'package:calorie_counter/data/local/entity/meal_nutrients.dart';
 import 'package:calorie_counter/injection.dart';
+import 'package:calorie_counter/ui/widgets/list_row/meal_summary_view.dart';
 import 'package:calorie_counter/util/constant/routes.dart';
-import 'package:calorie_counter/util/extension/ext_meal_type_description.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
@@ -54,31 +54,6 @@ class DailySummaryScreen extends StatelessWidget {
         ],
       ),
     );
-  }
-
-  void _showMealFoodListScreen(
-      BuildContext context, MealNutrients mealNutrients, DailySummaryBloc bloc) async {
-    await Navigator.of(context)
-        .push(MaterialPageRoute(
-            builder: (context) => MealFoodListScreen(mealNutrients),
-            settings: RouteSettings(name: Routes.mealFoodListScreen, arguments: Map())))
-        .then((val) {
-      var dateTime = DateFormat("EEEE, MMM d, yyyy").parse(val);
-      bloc.add(LoadTotalNutrientsEvent(dateTime));
-    });
-  }
-
-  void _buildDatePicker(BuildContext context, DateTime dateTime, DailySummaryBloc bloc) async {
-    final DateTime pickedDate = await showDatePicker(
-        context: context,
-        initialDate: dateTime,
-        firstDate: DateTime(1920),
-        lastDate: DateTime(2120));
-
-    if (pickedDate != null) {
-      bloc.add(ChangeTimeEvent(pickedDate));
-      bloc.add(LoadTotalNutrientsEvent(pickedDate));
-    }
   }
 
   Widget _buildAppBarDate(BuildContext context, DailySummaryBloc bloc) {
@@ -196,175 +171,37 @@ class DailySummaryScreen extends StatelessWidget {
                 isShowChartIfEmpty: true));
       }
 
+      var currentMealNutrients = mealNutrients[index - 1];
       return Container(
           margin: EdgeInsets.symmetric(vertical: 8.0),
-          child: _buildMealSummary(context, bloc, mealNutrients[index - 1]));
+          child: MealSummaryView(
+              mealNutrients: currentMealNutrients,
+              onTap: () => _showMealFoodListScreen(context, currentMealNutrients, bloc)));
     });
   }
 
-  Widget _buildMealSummary(
-      BuildContext context, DailySummaryBloc bloc, MealNutrients mealNutrients) {
-    return NeumorphicButton(
-      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      onClick: () => _showMealFoodListScreen(context, mealNutrients, bloc),
-      style: NeumorphicStyle(
-        shape: NeumorphicShape.convex,
-        shadowDarkColorEmboss: Color.fromRGBO(163, 177, 198, 1),
-        shadowLightColorEmboss: Colors.white,
-        color: Color.fromRGBO(193, 214, 233, 1),
-      ),
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('${mealNutrients.type.description()}',
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w700,
-                    )),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: FittedBox(
-                      child: Text('${mealNutrients.calories}',
-                          style: TextStyle(
-                            fontFamily: 'Roboto',
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.w700,
-                          )),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Flex(
-            direction: Axis.horizontal,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Expanded(
-                flex: 9,
-                child: Flex(
-                  direction: Axis.horizontal,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Container(
-                          padding: EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
-                          child: Column(
-                            children: <Widget>[
-                              FittedBox(
-                                child: Text(
-                                  '${mealNutrients.carbs}g',
-                                  style: TextStyle(
-                                    fontFamily: 'Roboto',
-                                    fontSize: 24.0,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                'Carbs',
-                                style: TextStyle(
-                                  fontFamily: 'Roboto',
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Container(
-                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-                          child: Column(
-                            children: <Widget>[
-                              FittedBox(
-                                child: Text(
-                                  '${mealNutrients.fat}g',
-                                  style: TextStyle(
-                                    fontFamily: 'Roboto',
-                                    fontSize: 24.0,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                'Fat',
-                                style: TextStyle(
-                                  fontFamily: 'Roboto',
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Container(
-                          padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-                          child: Column(
-                            children: <Widget>[
-                              FittedBox(
-                                child: Text(
-                                  '${mealNutrients.protein}g',
-                                  style: TextStyle(
-                                    fontFamily: 'Roboto',
-                                    fontSize: 24.0,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                'Protein',
-                                style: TextStyle(
-                                  fontFamily: 'Roboto',
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Neumorphic(
-                  boxShape: NeumorphicBoxShape.circle(),
-                  child: Icon(Icons.chevron_right),
-                  style: NeumorphicStyle(
-                    shape: NeumorphicShape.convex,
-                    shadowDarkColorEmboss: Color.fromRGBO(163, 177, 198, 1),
-                    shadowLightColorEmboss: Colors.white,
-                    color: Color.fromRGBO(193, 214, 233, 1),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ],
-      ),
-    );
+  void _showMealFoodListScreen(
+      BuildContext context, MealNutrients mealNutrients, DailySummaryBloc bloc) async {
+    await Navigator.of(context)
+        .push(MaterialPageRoute(
+            builder: (context) => MealFoodListScreen(mealNutrients),
+            settings: RouteSettings(name: Routes.mealFoodListScreen, arguments: Map())))
+        .then((val) {
+      var dateTime = DateFormat("EEEE, MMM d, yyyy").parse(val);
+      bloc.add(LoadTotalNutrientsEvent(dateTime));
+    });
+  }
+
+  void _buildDatePicker(BuildContext context, DateTime dateTime, DailySummaryBloc bloc) async {
+    final DateTime pickedDate = await showDatePicker(
+        context: context,
+        initialDate: dateTime,
+        firstDate: DateTime(1920),
+        lastDate: DateTime(2120));
+
+    if (pickedDate != null) {
+      bloc.add(ChangeTimeEvent(pickedDate));
+      bloc.add(LoadTotalNutrientsEvent(pickedDate));
+    }
   }
 }
---
+++ lib/ui/widgets/list_row/meal_summary_view.dart
@@ -0,0 +1,125 @@
+import 'package:calorie_counter/data/local/entity/meal_nutrients.dart';
+import 'package:calorie_counter/util/extension/ext_meal_type_description.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class MealSummaryView extends StatelessWidget {
+  MealSummaryView({Key key, this.mealNutrients, this.onTap});
+
+  final MealNutrients mealNutrients;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicButton(
+      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      onClick: onTap,
+      style: NeumorphicStyle(
+        shape: NeumorphicShape.convex,
+        shadowDarkColorEmboss: Color.fromRGBO(163, 177, 198, 1),
+        shadowLightColorEmboss: Colors.white,
+        color: Color.fromRGBO(193, 214, 233, 1),
+      ),
+      child: Column(
+        children: <Widget>[
+          _buildMealTypeAndCalorie(),
+          Flex(
+            direction: Axis.horizontal,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                flex: 9,
+                child: Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    _buildNutrient('Carbs', mealNutrients.carbs),
+                    _buildNutrient('Fat', mealNutrients.fat),
+                    _buildNutrient('Protein', mealNutrients.protein)
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Neumorphic(
+                  boxShape: NeumorphicBoxShape.circle(),
+                  child: Icon(Icons.chevron_right),
+                  style: NeumorphicStyle(
+                    shape: NeumorphicShape.convex,
+                    shadowDarkColorEmboss: Color.fromRGBO(163, 177, 198, 1),
+                    shadowLightColorEmboss: Colors.white,
+                    color: Color.fromRGBO(193, 214, 233, 1),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMealTypeAndCalorie() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text('${mealNutrients.type.description()}',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+              )),
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: FittedBox(
+                child: Text('${mealNutrients.calories}',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w700,
+                    )),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNutrient(String mealType, int nutrientCount) {
+    return Expanded(
+      flex: 1,
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Container(
+          padding: EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+          child: Column(
+            children: <Widget>[
+              FittedBox(
+                child: Text(
+                  '${nutrientCount}g',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              Text(
+                mealType,
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
